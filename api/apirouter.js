f63cf6d8d1f92bd0673086b16048a5eb44d44e4e
Samuel Schmid <samuel.schmid@namics.com>
2014-02-16 12:58:38 +0100

    separate api routes

---
+++ api/apirouter.js
@@ -0,0 +1,36 @@
+exports.get = function (req, res) {
+
+    var tmpPath = [];
+    var ressource = {
+        "type":undefined,
+        "version":undefined,
+        "pathRessources":[],
+        "ids": [],
+        "path": undefined,
+        "restlet": undefined
+
+    };
+
+    req.params[0].match(/(\/(\w+))/g).map(function (e) {
+        tmpPath.push(e.substring(1));
+
+    })
+    ressource.version = tmpPath[0];
+    tmpPath.splice(0,1);
+    tmpPath.map(function(e,index){
+        (index%2==0) ? ressource.pathRessources.push(e) : ressource.ids.push(e);
+    });
+
+    (tmpPath.length%2 == 0) ? ressource.type = 'instance' : ressource.type = 'collection';
+    ressource.path = ressource.pathRessources.join('/') + '/';
+    ressource.restlet = ressource.pathRessources.join('/') + '/_get_' + ressource.type + '_'  + ressource.version + '.js';
+
+    // load restlet documentation file
+
+    // prove attribute constraints
+
+    // load and execute restlet
+
+
+    res.send(ressource);
+};
--- app.js
+++ app.js
@@ -5,7 +5,7 @@
 var express = require('express');
 var http = require('http');
 var path = require('path');
-
+var apirouter = require('./api/apirouter');
 var app = express();
 
 // all environments
@@ -25,43 +25,7 @@ if ('development' == app.get('env')) {
     app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
-app.get('*', function (req, res) {
-
-    var tmpPath = [];
-    var ressource = {
-        "type":undefined,
-        "version":undefined,
-        "pathRessources":[],
-        "ids": [],
-        "path": undefined,
-        "restlet": undefined
-
-    };
-
-    req.params[0].match(/(\/(\w+))/g).map(function (e) {
-        tmpPath.push(e.substring(1));
-
-    })
-    ressource.version = tmpPath[0];
-    tmpPath.splice(0,1);
-    tmpPath.map(function(e,index){
-        (index%2==0) ? ressource.pathRessources.push(e) : ressource.ids.push(e);
-    });
-
-    (tmpPath.length%2 == 0) ? ressource.type = 'instance' : ressource.type = 'collection';
-    ressource.path = ressource.pathRessources.join('/') + '/';
-    ressource.restlet = ressource.pathRessources.join('/') + '/_get_' + ressource.type + '_'  + ressource.version + '.js';
-
-    // load restlet documentation file
-
-    // prove attribute constraints
-
-    // load and execute restlet
-
-
-    res.send(ressource);
-});
+app.get('*', apirouter.get);
 
 http.createServer(app).listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
