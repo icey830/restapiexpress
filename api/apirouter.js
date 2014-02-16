266ba5dd852eb2a0eb3d1d7965a4c5c729554c96
Veith Zäch <veithz@gmail.com>
2014-02-16 13:58:35 +0100

    route

--- api/apirouter.js
+++ api/apirouter.js
@@ -1,15 +1,16 @@
-var fs= require('fs');
+var fs = require('fs');
 var apirouter = {};
 
-apirouter.route = function(req, res, verb) {
- 	var tmpPath = [];
-    var ressource = {
-        "type":undefined,
-        "version":undefined,
-        "pathRessources":[],
+apirouter.route = function (req, res) {
+    var tmpPath = [];
+    var resource = {
+        "type": undefined,
+        "version": undefined,
+        "pathResources": [],
         "ids": [],
         "path": undefined,
-        "restlet": undefined
+        "restlet": undefined,
+        "method" : req.method
 
     };
 
@@ -17,29 +18,30 @@ apirouter.route = function(req, res, verb) {
         tmpPath.push(e.substring(1));
 
     })
-    ressource.version = tmpPath[0];
-    tmpPath.splice(0,1);
-    tmpPath.map(function(e,index){
-        (index%2==0) ? ressource.pathRessources.push(e) : ressource.ids.push(e);
+    resource.version = tmpPath[0];
+    tmpPath.splice(0, 1);
+    tmpPath.map(function (e, index) {
+        (index % 2 == 0) ? resource.pathResources.push(e) : resource.ids.push(e);
     });
 
-    (tmpPath.length%2 == 0) ? ressource.type = 'instance' : ressource.type = 'collection';
-    ressource.path = ressource.pathRessources.join('/') + '/';
-    ressource.restlet = ressource.pathRessources.join('/') + '/_'+verb+'_' + ressource.type + '_'  + ressource.version + '.js';
+    (tmpPath.length % 2 == 0) ? resource.type = 'instance' : resource.type = 'collection';
+    resource.path = resource.pathResources.join('/') + '/';
+    resource.restlet = resource.pathResources.join('/') + '/_' + req.method + '_' + resource.type + '_' + resource.version + '.js';
 
-	// send 404 if no documentation
-	
+    // send 404 if no documentation
+
     // load restlet documentation file
 
     // prove attribute constraints
 
     // load and execute restlet
-	var restlet = require('./' + ressource.restlet);
-	restlet.send(req, res, ressource);
-};
-exports.get = function (req, res) {
 
-   apirouter.route(req, res, 'get');
-    
+    var restlet = require('./' + resource.restlet);
+    restlet.send(req, res, resource);
+};
+exports.route = function (req, res) {
+
+    apirouter.route(req, res);
+
 };
 
