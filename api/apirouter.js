0566f8d734e02b37780b0bb946b98584ac80df45
Samuel Schmid <samuel.schmid@namics.com>
2014-02-21 16:42:03 +0100

    improves stability of server with checking of file existance on server

--- api/apirouter.js
+++ api/apirouter.js
@@ -3,38 +3,83 @@ var apirouter = {};
 var Resource = require('./resource');
 var config = require('./config.js');
 var util = require('util');
+var fs = require('fs');
+
+
 apirouter.route = function (req, res) {
-   
+
     var resource = new Resource(req);
-	
-    var restlet = require('./' + resource.restlet);
-    var doc = require('./' + resource.documentation);
-    // TBD:: define CORS on a per restlet basis and how do we handle Access-Control-Allow-Method ?
-    res.header('Access-Control-Allow-Origin', config.CORSAllowOrigin);
 
-    // OPTIONS
-    // TODO:: send  documentation for implemented verbs as json  we will not violate RFC2616 http://www.w3.org/Protocols/rfc2616/rfc2616-sec9.html
-    //
-    // @ssNamics this is a express problem
-    // see https://github.com/visionmedia/express/pull/772/files
-    // see https://groups.google.com/forum/#!topic/express-js/0oNFpyH51_k
-    // OPTIONS
+    var loadRestlet = function(restlet) {
 
-    // load restlet documentation file
+        // TBD:: define CORS on a per restlet basis and how do we handle Access-Control-Allow-Method ?
+        res.header('Access-Control-Allow-Origin', config.CORSAllowOrigin);
 
-    // send 404 if no documentation
+        // OPTIONS
+        // TODO:: send  documentation for implemented verbs as json  we will not violate RFC2616 http://www.w3.org/Protocols/rfc2616/rfc2616-sec9.html
+        //
+        // @ssNamics this is a express problem
+        // see https://github.com/visionmedia/express/pull/772/files
+        // see https://groups.google.com/forum/#!topic/express-js/0oNFpyH51_k
+        // OPTIONS
 
-    // prove attribute constraints
+        //@veith
+        //note that require only reads the file once, following calls return the result from cache
 
-    // load and execute restlet
+        console.log(util.inspect(resource,false,null));
 
-    // find implementations for documented verbs
+        var sendRestlet = function(doc) {
 
-	console.log(util.inspect(resource,false,null));
-    console.log("Doc:");
-    console.log(util.inspect(doc,false,null));
-    restlet.send(req, res, resource);
+            console.log("Doc:");
+            console.log(util.inspect(doc,false,null));
+
+            // TODO prove attribute constraints
+
+            // load and execute restlet
+
+            // TODO find implementations for documented verbs
+
+            restlet.send(req, res, resource);
+
+        };
+        apirouter.getFileFromFileSystem(resource.documentation, res, sendRestlet);
+    }
+    apirouter.getFileFromFileSystem(resource.restlet, res, loadRestlet);
+
+
+
+
+
 };
+apirouter.getFileFromFileSystem = function (filePath, res, callback) {
+    if(filePath == undefined) {
+        // send 404 if no documentation
+        res.status(404).send('Not found');
+    } else {
+        var path = __dirname + '/'+ filePath;
+        console.log(path);
+        fs.exists(path, function (exists) {
+            if(exists) {
+                // load restlet documentation file
+                var file = require('./' + filePath);
+
+                if (file != undefined) {
+                    callback(file);
+
+                } else {
+                    // send 500 if  documentation file corrupt
+                    res.status(500).send('documentation file corrupt');
+                }
+            } else {
+                // send 404 if no documentation file on server
+                res.status(404).send('Not found no file');
+            }
+        });
+
+    }
+
+}
+
 
 exports.route = function (req, res) {
     apirouter.route(req, res);
