c9d7a2954b73865c9bcea50717a8193e968dd875
Samuel Schmid <samuel.schmid@namics.com>
2014-03-15 13:50:22 +0100

    refactors api router for dev mode only methods

--- app.js
+++ app.js
@@ -23,11 +23,12 @@ app.use(express.methodOverride());
 if ('development' == app.get('env')) {
     app.use(express.errorHandler());
 }
+
 app.all('/', apirouter.apidescription);
 app.all('*', apirouter.route);
 
 http.createServer(app).listen(app.get('port'), function () {
-    console.log('Express server listening on port ' + app.get('port'));
+    console.log('Express server listening on port ' + app.get('port') + ' in ' +app.get('env') + ' mode');
 });
 
 module.exports = app;
