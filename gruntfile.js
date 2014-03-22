c6d0d5dd1e2e525872769464a6d345698e1aa56d
Samuel Schmid <samuel.schmid@namics.com>
2014-03-22 10:56:12 +0100

    adds config file for api, installs config dependecies for db with grunt setup

--- gruntfile.js
+++ gruntfile.js
@@ -8,19 +8,33 @@ module.exports = function(grunt){
 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-
+        appconfig: grunt.file.readJSON('config.json'),
     });
 
-    grunt.test = function() {
 
-        var done = this.async();
-        require('child_process').exec('mocha-phantomjs ./tests/index.html', function (err, stdout) {
-            grunt.log.write(stdout);
-            done(err);
-        });
-    }
 
     grunt.registerTask('default', 'searchDocs', function() {
+
+        var dependencies = grunt.config().pkg.dependencies;
+        var appconfig = grunt.config().appconfig;
+        //
+      //  grunt.log.debug(JSON.stringify(appconfig));
+
+
+        for (var key in appconfig.db.dependencies) {
+            if ( appconfig.db.dependencies.hasOwnProperty(key)) {
+
+                if(dependencies.hasOwnProperty(key)) {
+                    grunt.log.debug("extension " +key +":" + JSON.stringify(appconfig.db.dependencies[key]));
+                    grunt.log.debug("package * " + key +":"+ JSON.stringify(dependencies[key]));
+                    //TODO install dependencies of db if newer version in config
+                } else {
+                    //TODO install dependencies
+                    grunt.log.debug("install extension " +key +":" + JSON.stringify(appconfig.db.dependencies[key]));
+                }
+
+            }
+        }
 
         var allDocuments = new Docs(grunt);
 
@@ -89,5 +103,70 @@ module.exports = function(grunt){
         });
     });
 
+    grunt.registerTask('setup', 'install extensions', function() {
+
+        var dependencies = grunt.config().pkg.dependencies;
+        var appconfig = grunt.config().appconfig;
+        //
+        //  grunt.log.debug(JSON.stringify(appconfig));
+
+
+
+        var asyncTasks=[];
+        for (var key in appconfig.db.dependencies) {
+            if ( appconfig.db.dependencies.hasOwnProperty(key)) {
+
+                if(dependencies.hasOwnProperty(key)) {
+                    grunt.log.debug("extension " +key +":" + JSON.stringify(appconfig.db.dependencies[key]));
+                    grunt.log.debug("package * " + key +":"+ JSON.stringify(dependencies[key]));
+                    //TODO install dependencies of db if newer version in config
+                } else {
+
+                    var version = appconfig.db.dependencies[key];
+                    if(version === "*") {
+                        version = "latest";
+                    }
+                    asyncTasks.push(
+                        {
+                            cmd:'npm',
+                            args: ['install', key + "@" + version]
+                        }
+                    );
+
+                }
+
+            }
+        }
+
+        var done = this.async();
+        var next = function(index){
+            var task = asyncTasks[index];
+            grunt.log.write("\ninstall extension \n" + asyncTasks[index].args[1] );
+            grunt.util.spawn(task, function(error,result) {
+                grunt.log.write(result);
+
+                if(error) {
+
+                    done(error)
+                } else {
+                    if(asyncTasks.length > ++index) {
+
+                        next(index);
+                    } else {
+
+                        done(error);
+                    }
+                }
+
+            });
+        }
+        if(asyncTasks.length > 0) {
+            var index = 0;
+            next(index);
+
+        }
+
+
+    });
 
 };
