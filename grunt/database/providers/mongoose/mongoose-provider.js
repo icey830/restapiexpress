a48528146c06c86cff2c31af40e568b554080290
Samuel Schmid <samuel.schmid@namics.com>
2014-03-28 11:52:21 +0100

    adds default values to scheme types

--- grunt/database/providers/mongoose/mongoose-provider.js
+++ grunt/database/providers/mongoose/mongoose-provider.js
@@ -8,6 +8,16 @@ String.prototype.replaceAll = function(target, replacement) {
 String.prototype.capitalize = function() {
     return this.charAt(0).toUpperCase() + this.slice(1);
 }
+function objToString (obj) {
+    var str = '{\n';
+    for (var p in obj) {
+        if (obj.hasOwnProperty(p)) {
+            str += p + ':' + obj[p] + ',\n';
+        }
+    }
+    str = str.substring(0,str.length-2);
+    return str + "\n}";
+}
 function MongooseProvider(grunt) {
     this.grunt = grunt;
 }
@@ -16,15 +26,25 @@ MongooseProvider.prototype.createSchemeAndGetLibFile = function(doc)  {
     this.grunt.log.debug("create Scheme for: " + doc.json.title);
 
     var template = this.grunt.file.read('./grunt/database/providers/mongoose/scheme.template');
+    var types = this.grunt.file.read('./grunt/database/providers/mongoose/types.template');
     template = template.replaceAll("{{{NAME}}}",doc.json.singular);
-
+    template = template.replace("{{{TYPES}}}",types);
     var model = doc.json.model;
     var scheme = {};
     for (var key in model) {
-        scheme[key] = model[key].type.capitalize();
+
+        var type= "{type: ";
+        type +=  model[key].type.capitalize();
+        if(model[key].default) {
+
+            type += ", default: " + JSON.stringify(model[key].default);
+        }
+        type += "}";
+        scheme[key] = type;
     }
-    scheme["type"] = "String";
-    template = template.replaceAll("{{{SCHEME}}}",JSON.stringify(scheme));
+    scheme["type"] = "{type: String}";
+
+    template = template.replaceAll("{{{SCHEME}}}",objToString(scheme));
 
     this.grunt.file.write(doc.schemefolder+ doc.json.singular+'.js', template);
 
