d451a75226db5f4b1aae463d14bc7454b5f9c5aa
Samuel Schmid <samuel.schmid@namics.com>
2014-08-12 16:02:32 +0200

    remove access headers in router

--- app.js
+++ app.js
@@ -17,8 +17,9 @@ app.set('port', process.env.PORT || 3000);
 
 var allowCrossDomain = function(req, res, next) {
     //res.header('Access-Control-Allow-Origin', config.allowedDomains);
-    res.header('Access-Control-Allow-Origin', "*");
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Origin', "http://club.app");
+    res.header('Access-Control-Allow-Credentials', "true")
+    res.header('Access-Control-Allow-Methods', '*');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
 
     next();
